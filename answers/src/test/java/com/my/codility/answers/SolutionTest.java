a2a7267cfe36745611cb13975ceb2679c55631c2
Ishara <isharafe@gmail.com>
2024-11-15 00:14:05 +0800

    Update SolutionTest.java

--- answers/src/test/java/com/my/codility/answers/SolutionTest.java
+++ answers/src/test/java/com/my/codility/answers/SolutionTest.java
@@ -10,6 +10,13 @@ public class SolutionTest {
 	@Test
 	public void testSolution() {
 		Solution s = new Solution();
-
+		System.out.println(s.add("1,2,2"));
+	        System.out.println(s.add("1,2,\n3"));
+	        System.out.println(s.add("1,\n2"));
+	        System.out.println(s.add("1\n,2"));   // should fail
+	        System.out.println(s.add("//**\n1**2**3"));
+	        System.out.println(s.add("//**|::|***|,\n1,2**3::4***5"));
+	        System.out.println(s.add("1, 2, 3"));
+		System.out.println(app.add("1,2,-3,4,-5,6"));	// should fail
 	}
 }
